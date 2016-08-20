976758778013d2752a80bf28fe2d556ad03170c3
Tom Martin <tom@tpgmartin.com>
2016-08-20 15:14:43 +0100

    Add react-redux, create smart/dumb components

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
 import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import reducer from './reducer'
 import App from './components/App'
-import Voting from './components/Voting'
-import Results from './components/Results'
+import {VotingContainer} from './components/Voting'
+import {ResultsContainer} from './components/Results'
 
 const store = createStore(reducer)
 store.dispatch({
@@ -19,11 +20,13 @@ store.dispatch({
 })
 
 const routes = <Route component={App}>
-  <Route path="/results" component={Results} />
-  <Route path="/" component={Voting} />
+  <Route path="/results" component={ResultsContainer} />
+  <Route path="/" component={VotingContainer} />
 </Route>
 
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 )
