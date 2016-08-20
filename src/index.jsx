42d74a95301dab06ce39c84fb51025e111e8a29e
Tom Martin <tom@tpgmartin.com>
2016-08-20 16:17:59 +0100

    Send actions to server

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import io from 'socket.io-client'
 import reducer from './reducer'
 import {setState} from './actionCreators'
+import remoteActionMiddleware from './remoteActionMiddleware'
 import App from './components/App'
 import {VotingContainer} from './components/Voting'
 import {ResultsContainer} from './components/Results'
 
-const store = createStore(reducer)
-
-const socket = io(`${location.protocol}//${Location.hostname}:8090`)
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
 socket.on('state', state => 
   store.dispatch(setState(state))
 )
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore)
+const store = createStoreWithMiddleware(reducer)
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
